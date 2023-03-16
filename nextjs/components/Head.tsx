a5421d46aeb5d942bead1cb6d9c5f990eaf5f7cb
Dom Vinyard <domvinyard@me.com>
2023-03-16 21:23:38 +0000

    Update Head.tsx

--- nextjs/components/Head.tsx
+++ nextjs/components/Head.tsx
@@ -1,21 +1,21 @@
 import Head from "next/head";
 
 export function TopicHead(props: any) {
+  const description =
+    props.descriptions?.[0]?.extra_short || "Generating Description";
   return (
     <>
       <Head>
-        <title>{props.name}</title>
+        <title>Explain {props.name}</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content={
-            props.descriptions?.[0]?.extra_short || "Generating Description"
-          }
-        />
+        <meta name="description" content={description} />
         <meta name="author" content="Sigma Labs" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="image" content={props.image} />
         <link rel="shortcut icon" href="/favicon.ico" />
+        <meta property="og:site_name" content={"ExplainAI"} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={props.image} />
       </Head>
     </>
   );
@@ -27,6 +27,7 @@ export function GroupsHead() {
       <Head>
         <title>ExplainAI.me</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:site_name" content={"ExplainAI"} />
         <meta name="description" content={"Explain it to me. Simply."} />
         <meta name="author" content="Sigma Labs" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
