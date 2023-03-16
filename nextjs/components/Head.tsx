4033f0c8ea36e7fa660d17b8a8c84864006044ae
Dom Vinyard <domvinyard@me.com>
2023-03-16 21:25:40 +0000

    Update Head.tsx

--- nextjs/components/Head.tsx
+++ nextjs/components/Head.tsx
@@ -12,7 +12,6 @@ export function TopicHead(props: any) {
         <meta name="author" content="Sigma Labs" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="image" content={props.image} />
-        <link rel="shortcut icon" href="/favicon.ico" />
         <meta property="og:site_name" content={"ExplainAI"} />
         <meta property="og:description" content={description} />
         <meta property="og:image" content={props.image} />
@@ -25,13 +24,12 @@ export function GroupsHead() {
   return (
     <>
       <Head>
-        <title>ExplainAI.me</title>
+        <title>ExplainAI</title>
         <link rel="icon" href="/favicon.ico" />
         <meta property="og:site_name" content={"ExplainAI"} />
         <meta name="description" content={"Explain it to me. Simply."} />
         <meta name="author" content="Sigma Labs" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
     </>
   );
