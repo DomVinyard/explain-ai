e09ea7060a8ea9e9198b1730193fff5b04555849
Dom Vinyard <domvinyard@me.com>
2023-03-16 21:06:11 +0000

    delete bin

--- nextjs/components/Group.tsx
+++ nextjs/components/Group.tsx
@@ -4,7 +4,7 @@ import TopicMini from "./TopicMini";
 export default function Group({ group, audience }: any) {
   if (!group) return null;
   return (
-    <>
+    <div className={styles.container}>
       <h1 className={styles.h1}>
         {group.name}{" "}
         <span className={styles.description}>{group.description}</span>
@@ -26,6 +26,6 @@ export default function Group({ group, audience }: any) {
           </div>
         ))}
       </div>
-    </>
+    </div>
   );
 }
--- nextjs/components/Header.tsx
+++ nextjs/components/Header.tsx
@@ -11,11 +11,18 @@ export default function Header({ slug, audience, page }: any) {
       <Inner>
         <div className={styles.container}>
           <Link href={`/groups/${audience}`}>
-            <Image src="/logo.png" alt="Logo" width={200} height={50} />
+            <Image
+              className={styles.pointer}
+              src="/logo.png"
+              alt="Logo"
+              width={200}
+              height={50}
+            />
           </Link>
           <Link href={`/${pagePath}/${audiencePath}`}>
-            <div className={styles.toggle}>
+            <div>
               <Image
+                className={styles.pointer}
                 src={`/${audience === "5" ? "checked" : "unchecked"}.png`}
                 alt="Switch"
                 width={120}
