546fd219032dc312750e04f621038a63252e8c66
Dom Vinyard <domvinyard@me.com>
2023-03-17 23:43:20 +0000

    Update Stub.tsx

--- nextjs/components/Stub.tsx
+++ nextjs/components/Stub.tsx
@@ -21,7 +21,7 @@ export default function Stub(props: any) {
         const { success, reload, error } = await response.json();
         if (success || reload) {
           setIsLoading(false);
-          setTimeout(() => location.reload(), 100);
+          setTimeout(() => location.reload(), 1000);
         } else {
           setError(error);
           return;
