8ee39966837c8ef44a129f2831b4258990f99ca4
Dom Vinyard <domvinyard@me.com>
2023-03-17 15:04:06 +0000

    Update Head.tsx

--- nextjs/components/Head.tsx
+++ nextjs/components/Head.tsx
@@ -3,7 +3,8 @@ import Head from "next/head";
 export function TopicHead(props: any) {
   const description =
     props.descriptions?.[0]?.extra_short || "Generating Description";
-  const audience = props.audience === 5 ? ` like I'm 5` : "";
+  console.log({ props });
+  const audience = props.audience === "5" ? ` like I'm 5` : "";
   const title = `Explain ${props.name}${audience}`;
   const image = props?.image || "/placeholder.png";
   return (
